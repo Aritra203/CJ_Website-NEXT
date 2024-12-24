a7780a9fe15adfd1308189a6bf64cc1dc542f752
Aritra203 <konararitra72@gmail.com>
2024-12-24 18:17:42 +0530

    --day2 init

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Coding Junction Club</h1>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Coding Junction Club
+      </h1>
     </div>
   );
 }
